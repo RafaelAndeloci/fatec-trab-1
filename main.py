a93a19dccc8e1adf33258e27501d058bced0d6c8
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2026-09-18 17:57:57 -0300

    ajustes no main

--- main.py
+++ main.py
@@ -4,19 +4,33 @@ import runpy
 diretorio_projeto = Path(__file__).resolve().parent
 
 
-def executar_exercicios_1_e_2():
+def executar_exercicios_1():
     """Executa os exercícios de inspeção e tratamento dos dados."""
+    print("===========================ETAPA 1===========================")
+    runpy.run_path(diretorio_projeto / "exercicio-1.py", run_name="__main__")
+
+
+def executar_exercicios_2():
+    print("===========================ETAPA 2===========================")
     runpy.run_path(diretorio_projeto / "exercicio-2.py", run_name="__main__")
 
 
-def executar_exercicios_3_e_4():
-    """Executa os exercícios de agrupamento, agregação e visualização."""
+def executar_exercicios_3():
+    print("===========================ETAPA 3===========================")
     runpy.run_path(diretorio_projeto / "exercicio-3.py", run_name="__main__")
 
 
+def executar_exercicios_4():
+    print("===========================ETAPA 4===========================")
+    print("(Exibido na janela aberta)")
+    runpy.run_path(diretorio_projeto / "exercicio-4.py", run_name="__main__")
+
+
 def main():
-    executar_exercicios_1_e_2()
-    executar_exercicios_3_e_4()
+    executar_exercicios_1()
+    executar_exercicios_2()
+    executar_exercicios_3()
+    executar_exercicios_4()
 
 
 if __name__ == "__main__":
